880d57aba721a0e628942fc105051438b2c3e6e6
kampus <kampus@Homes-MacBook-Pro.local>
2023-01-06 23:17:57 +0300

    Initial commit

---
+++ src/main/java/ru/otus/homework/OtusSpringHomework1Application.java
@@ -0,0 +1,13 @@
+package ru.otus.homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtusSpringHomework1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OtusSpringHomework1Application.class, args);
+    }
+
+}
